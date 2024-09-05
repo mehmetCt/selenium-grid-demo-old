210fd6b8849949cd0e24774e233b4d61d0cc5232
mehmetCt <85017903+mehmetCt@users.noreply.github.com>
2024-09-05 17:52:19 -0400

    Update Driver.java
    New SG IP

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -53,7 +53,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.80.83.183";
+                        String gridAddress = "18.204.7.149";
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--start-maximized");
